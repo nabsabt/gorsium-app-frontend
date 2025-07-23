eaf0f62f43cc6214a46567f4d457c3fae434574a
nabsabt <nabsabt@gmail.com>
2025-07-23 15:04:24 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+function setRealViewportHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setRealViewportHeight();
+window.addEventListener('resize', setRealViewportHeight);
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
