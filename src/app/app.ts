098350dda7c694edb3423feaf02e7d56b1afd785
nabsabt <nabsabt@gmail.com>
2025-08-22 09:51:29 +0200

    added warning alert when loading

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,5 +10,10 @@ import { NavbarComponent } from './@Component/navbar/navbar.component';
   standalone: true,
 })
 export class App {
+  constructor() {
+    alert(
+      ' Mivel az oldal tartalmát jelenleg a Render Dashboard ingyenes verzió tölti be, így első megnyitáskor körülbelül 30 mp-et kell várni a média tartalmak betöltésére.'
+    );
+  }
   protected readonly title = signal('gorsium-app-frontend');
 }
